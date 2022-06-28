fce6b9eb88d1fb51d5565f11fb3b8f48631ba2a1
JuanM04 <me@juanm04.com>
2022-06-28 15:16:44 -0300

    Reproduction

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import { Root } from '@/Root'
+import { Root } from '@/Root.js'
 import '@/styles.css'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
